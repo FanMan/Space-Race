9cee8f15a289f00c468115e28893135a8d039601
LilJunJun <goninar@gmail.com>
2017-04-17 22:51:37 -0400

    Checkpoints-Laps-GalaxyMap -Ships
    Galaxy Map is complete.  Checkpoints and Laps work for all ships.

--- Space Race/Assets/_Scripts/CheckPoint.cs
+++ Space Race/Assets/_Scripts/CheckPoint.cs
@@ -1,15 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class CheckPoint : MonoBehaviour
+public class CheckPoint : MonoBehaviour 
 {
+	//for checkpoints
+	//public Transform[] checkPointArray;			//keeps track of which checkpoint the player has passed/is going to pass
+	//public static Transform[] checkpointA;		//stores the checkpoint
 
-	void OnTriggerEnter(Collider ShipObject)
-    {
-        if(ShipObject.CompareTag("Player"))
-        {
-            // increment the lap number
-        }
-    }
+	void  OnTriggerEnter (Collider other)
+	{
+		//checks if we are dealing with the player
+		if (!other.CompareTag("Player")) 
+			return; //if not get out of here
+		//check if we hit one of the checkpoints in the array
+		if (transform == Laps.checkpointA[Laps.currentCheckpoint].transform) 
+		{
+			//check to make sure we didnt go over how many checkpoints we set
+			if (Laps.currentCheckpoint + 1 < Laps.checkpointA.Length) 
+			{
+				//increment lap as player went through all checkpoints - yay
+				if(Laps.currentCheckpoint == 0)
+					Laps.currentLap++;
+				Laps.currentCheckpoint++;
+			} 
+			else 
+			{
+				//If we dont have any checkpoints left, reset to 0 so it can increment again
+				Laps.currentCheckpoint = 0;
+			}
+		}
+
+
+	}
+
 }
---
+++ Space Race/Assets/_Scripts/Laps.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Laps : MonoBehaviour 
+{
+	//for ships
+
+	public static int currentCheckpoint = 0; 	//what checkpoint we are currently on
+	public static int currentLap = 0; 			//what lap we are currently on
+	public int Lap;								//ship's lap count
+	public Transform[] checkPointArray;			//keeps track of which checkpoint the player has passed/is going to pass
+	public static Transform[] checkpointA;		//stores the checkpoint
+	public Vector3 initPos;						//ship's starting position
+
+	void  Start ()
+	{
+		//initial position of the ship when we start
+		initPos = transform.position;
+		//set these to 0 - this is where we start from the git-go
+		currentCheckpoint = 0;
+		currentLap = 0; 
+
+	}
+
+	void  Update ()
+	{
+		Lap = currentLap;
+		checkpointA = checkPointArray;
+
+		//if they completed 3 laps, go to the next map
+		if (Lap > 3) 
+		{
+			SceneManager.LoadScene(1);
+		}
+	}
+
+}
